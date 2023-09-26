5a31c6261179cb259183baeb6a5f5f021fac3518
francJi <minkyuji95@hanmail.net>
2023-09-26 21:04:32 +0900

    [ADD] MonsterAI 적용 시도중 (민규)

---
+++ Assets/Scripts/Test_Minkyu/EnemyRoad.cs
@@ -0,0 +1,146 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Tilemaps;
+
+public class EnemyRoad : MonoBehaviour
+{
+    [SerializeField] private GameObject Enemy;
+    [SerializeField] private GameObject Player;
+    [SerializeField] private Tilemap Tilemap;
+    private bool isDelay;
+
+    private Node[,] grid;
+
+    private enum Directions
+    {
+        Up = 0,
+        Down,
+        Left,
+        Right,
+        LeftUp,
+        RightUp,
+        LeftDown,
+        RightDown,
+    }
+
+    private Vector3[] Vector3Directions = new Vector3[] {
+        Vector3.up,
+        Vector3.down,
+        Vector3.left,
+        Vector3.right,
+        new Vector3(-1, 1, 0),
+        new Vector3(1, 1, 0),
+        new Vector3(-1, -1, 0),
+        new Vector3(1, -1, 0)
+    };
+    public void Update()
+    {
+        if (isDelay == false)
+        {
+            isDelay = true;
+            GetPosition();
+            StartCoroutine(CallPosPerSecond());
+        }
+    }
+    private void GetPosition()
+    {
+        Vector3 enemyPos = Enemy.transform.position;
+        Vector3 playerPos = Player.transform.position;
+
+        Debug.Log($"enemy : {Tilemap.WorldToCell(enemyPos)}");
+        Debug.Log($"player : {Tilemap.WorldToCell(playerPos)}");
+    }
+    private void CreateGrid()        // grid 배열화
+    {
+        Tilemap.CompressBounds();
+        BoundsInt bounds = Tilemap.cellBounds;
+        grid = new Node[bounds.size.y, bounds.size.x];
+        for (int y = bounds.yMin, i = 0; i < bounds.size.y; y++, i++)
+        {
+            for (int x = bounds.xMin, j = 0; j < bounds.size.x; x++, j++)
+            {
+                Node node = new Node();
+                node.yIndex = i;
+                node.xIndex = j;
+                node.gCost = int.MaxValue;
+                node.parent = null;
+                node.yPos = Tilemap.CellToWorld(new Vector3Int(x, y)).y;
+                node.xPos = Tilemap.CellToWorld(new Vector3Int(x, y)).x;
+                // walkable Tilemap에 타일이 있으면 이동 가능한 노드, 타일이 없으면 이동 불가능한 노드이다.
+                if (Tilemap.HasTile(new Vector3Int(x, y, 0)))
+                {
+                    node.isWalkable = true;
+                    grid[i, j] = node;
+                }
+                else
+                {
+                    node.isWalkable = false;
+                    grid[i, j] = node;
+                }
+            }
+        }
+    }
+
+    public Node GetNodeFromPos(Vector3 position)
+    {
+        Vector3Int cellPos = Tilemap.WorldToCell(position);
+        int y = cellPos.y + Mathf.Abs(Tilemap.cellBounds.yMin);
+        int x = cellPos.x + Mathf.Abs(Tilemap.cellBounds.xMin);
+
+        Node node = grid[y, x];
+        return node;
+    }
+
+    // gcost : 시작 노드부터 현재 노드까지의 가중치
+    // hcost : 현재 노트부터 끝 노드까지의 가중치
+    // fcost : gcost + hcost
+    private void GetNextNode(Node startNode, Node currentNode, Node endNode)
+    {
+        int newYindex;
+        int newXindex;
+        foreach ( Vector3 direction in Vector3Directions)
+        {
+            newYindex = (int)(currentNode.yIndex + direction.y);
+            newXindex = (int)(currentNode.xIndex + direction.x);
+            if (grid[newYindex,newXindex].isWalkable)
+            {
+                Node nextNode = grid[newYindex,newXindex];
+                if (nextNode.parent == null)
+                {
+                    nextNode.xIndex = newXindex;
+                    nextNode.yIndex = newYindex;
+                    getCurrentNodeCost(startNode, nextNode, endNode);
+                    nextNode.parent = currentNode;
+                }
+                else if (nextNode.)
+            }
+        }
+        
+    }
+    private void getCurrentNodeCost(Node startNode, Node currentNode, Node endNode)
+    {
+        currentNode.gCost = CalculateCost(startNode, currentNode);
+        currentNode.hCost = CalculateCost(currentNode, endNode);
+    }
+
+    private int CalculateCost(Node startNode, Node endNode)
+    {
+        int xDiff = Mathf.Abs(startNode.xIndex - endNode.xIndex);
+        int yDiff = Mathf.Abs(startNode.yIndex - endNode.yIndex);
+
+        int diagonalCost = Mathf.Min(xDiff, yDiff) * 14;
+        int straightCost = (Mathf.Max(xDiff, yDiff) - Mathf.Min(xDiff, yDiff)) * 10;
+
+        return diagonalCost + straightCost;
+    }
+
+
+    IEnumerator CallPosPerSecond()
+    {
+        yield return new WaitForSeconds(1.0f);
+        isDelay = false;
+    }
+}
---
+++ Assets/Scripts/Test_Minkyu/Node.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    public float xPos;
+    public float yPos;
+    public int xIndex;
+    public int yIndex;
+
+    public float hCost;
+    public float gCost;
+
+    public float fCost
+    {
+        get { return gCost + hCost; }
+    }
+
+    public Node parent;
+    public bool isWalkable;
+
+    public Node()
+    {
+        xPos = -1;
+        yPos = -1;
+        xIndex = -1;
+        yIndex = -1;
+        hCost = 0;
+        gCost = 0;
+        parent = null;
+    }
+
+    public void Reset()
+    {
+        hCost = 0;
+        gCost = int.MaxValue;
+        parent = null;
+    }
+
+    public Node CopyNode()
+    {
+        Node node = new Node();
+        node.xPos = this.xPos;
+        node.yPos = this.yPos;
+        node.xIndex = this.xIndex;
+        node.yIndex = this.yIndex;
+        node.hCost = this.hCost;
+        node.gCost = this.gCost;
+        node.parent = this.parent;
+        node.isWalkable = this.isWalkable;
+        return node;
+    }
+}
