f995ee2f989f8199d244ab92e02c78cb5687db1f
francJi <minkyuji95@hanmail.net>
2023-09-27 20:32:31 +0900

    [FIX] merge 오류 해결 (민규)

--- Assets/Scripts/Test_Minkyu/PermaDeath/ExpBar.cs
+++ Assets/Scripts/Test_Minkyu/PermaDeath/ExpBar.cs
@@ -69,22 +69,25 @@ public class ExpBar : MonoBehaviour
         slExp.value = (float)curExp / (float)maxExp;
         EXP_num.GetComponent<TextMeshProUGUI>().text = $"{curExp.ToString()}/{maxExp.ToString()}";
         currentExpArray = new int[] { curExp, maxExp, level };
-        calculateExpArray = CalculateExp(curExp, ExpEarned, maxExp, level);
+        //calculateExpArray = CalculateExp(curExp, ExpEarned, maxExp, level);
     }
 
     private int[] CalculateExp(int currentExp, int expEarned, int maxExp, int level)
     {
         while (currentExp + expEarned > maxExp )
         {
-            expEarned = currentExp + expEarned - maxExp;
-            level += 1;
-            currentExp = 0;
-            if (level > ExpTable.Length)
+            if (level >= ExpTable.Length)
             {
                 currentExp = 9999;
                 maxExp = 9999;
             }
-            maxExp = ExpTable[level - 1];
+            else
+            {
+                expEarned = currentExp + expEarned - maxExp;
+                level += 1;
+                currentExp = 0;
+                maxExp = ExpTable[level - 1];
+            }
         }
         int[] calculatedExpArray = new int[] { currentExp, maxExp, level };
         return calculatedExpArray;
