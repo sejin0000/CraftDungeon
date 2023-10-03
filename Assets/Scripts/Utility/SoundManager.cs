46c9ebb8bfe66445ff4e4ca947f00de949e84cf1
francJi <minkyuji95@hanmail.net>
2023-10-03 20:57:14 +0900

    [FIX] SoundManager 스크립트 제작중 (민규)

--- Assets/Scripts/Utility/SoundManager.cs
+++ Assets/Scripts/Utility/SoundManager.cs
@@ -8,7 +8,7 @@ public class SoundManager : MonoBehaviour
     
     AudioSource[] _audioSources = new AudioSource[(int)Sound.Max];
     Dictionary<string, AudioClip> _audioClips = new Dictionary<string, AudioClip>();
-    enum Sound
+    public enum Sound
     {
         Bgm,
         Effect,
@@ -28,8 +28,10 @@ public class SoundManager : MonoBehaviour
                 GameObject go = new GameObject { name = soundTypes[i] };
                 _audioSources[i] = go.AddComponent<AudioSource>();
                 go.transform.parent = root.transform;
+                GetOrAddAudioClip($"AudioSource/{soundTypes[i]}");
             }
 
+            _audioSources[(int)Sound.Bgm].loop = true;
         }
         else
         {
@@ -49,7 +51,7 @@ public class SoundManager : MonoBehaviour
         }
     }
 
-    AudioClip GetOrAddAudioClip(string path, Sound type = Sound.Max)
+    AudioClip GetOrAddAudioClip(string path, Sound type = Sound.Effect)
     {
         if (path.Contains("AudioSource/") == false)
             path = $"AudioSource/{path}"; 
@@ -73,5 +75,28 @@ public class SoundManager : MonoBehaviour
             Debug.Log($"AudioClip Missing ! {path}");
 
         return audioClip;
+    }
+
+    public void Play(AudioClip audioClip, Sound type = Sound.Effect, float pitch = 1.0f)
+    {
+        if (audioClip == null)
+            return;
+
+        if (type == Sound.Bgm) // BGM 배경음악 재생
+        {
+            AudioSource audioSource = _audioSources[(int)Sound.Bgm];
+            if (audioSource.isPlaying)
+                audioSource.Stop();
+
+            audioSource.pitch = pitch;
+            audioSource.clip = audioClip;
+            audioSource.Play();
+        }
+        else // Effect 효과음 재생
+        {
+            AudioSource audioSource = _audioSources[(int)Sound.Effect];
+            audioSource.pitch = pitch;
+            audioSource.PlayOneShot(audioClip);
+        }
     }
 }
