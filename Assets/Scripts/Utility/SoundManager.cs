da1d0c16169731b70017710f6137e8a5dffd9551
francJi <minkyuji95@hanmail.net>
2023-10-03 20:39:01 +0900

    [UPDATE] soundManager 수정 (민규)

--- Assets/Scripts/Utility/SoundManager.cs
+++ Assets/Scripts/Utility/SoundManager.cs
@@ -5,6 +5,15 @@ using UnityEngine;
 public class SoundManager : MonoBehaviour
 {
     private static SoundManager instance = null;
+    
+    AudioSource[] _audioSources = new AudioSource[(int)Sound.Max];
+    Dictionary<string, AudioClip> _audioClips = new Dictionary<string, AudioClip>();
+    enum Sound
+    {
+        Bgm,
+        Effect,
+        Max,
+    }
     void Awake()
     {
         if (null == instance)
@@ -12,6 +21,15 @@ public class SoundManager : MonoBehaviour
             instance = this;
 
             DontDestroyOnLoad(this.gameObject);
+            GameObject root = this.gameObject;
+            string[] soundTypes = System.Enum.GetNames(typeof(Sound));
+            for (int i = 0; i < soundTypes.Length - 1; i++)
+            {
+                GameObject go = new GameObject { name = soundTypes[i] };
+                _audioSources[i] = go.AddComponent<AudioSource>();
+                go.transform.parent = root.transform;
+            }
+
         }
         else
         {
@@ -29,5 +47,31 @@ public class SoundManager : MonoBehaviour
             }
             return instance;
         }
+    }
+
+    AudioClip GetOrAddAudioClip(string path, Sound type = Sound.Max)
+    {
+        if (path.Contains("AudioSource/") == false)
+            path = $"AudioSource/{path}"; 
+
+        AudioClip audioClip = null;
+
+        if (type == Sound.Max) // BGM 배경음악 클립 붙이기
+        {
+            audioClip =  Resources.Load<AudioClip>(path);
+        }
+        else // Effect 효과음 클립 붙이기
+        {
+            if (_audioClips.TryGetValue(path, out audioClip) == false)
+            {
+                audioClip = Resources.Load<AudioClip>(path);
+                _audioClips.Add(path, audioClip);
+            }
+        }
+
+        if (audioClip == null)
+            Debug.Log($"AudioClip Missing ! {path}");
+
+        return audioClip;
     }
 }
