be602ad89c9f379ffb64e9d04c03cacb09270f5d
Lyrwhitt <47979697+Lyrwhitt@users.noreply.github.com>
2023-10-03 16:55:15 +0900

    [Fix] 콜라이더 겹침으로 인한 맵이동 버그 수정

--- Assets/Scripts/Map/Door.cs
+++ Assets/Scripts/Map/Door.cs
@@ -5,6 +5,8 @@ using EnumManager;
 
 public class Door : MonoBehaviour
 {
+    private const float WARP_OFFSET = 1.2f;
+
     public DoorType doorType;
     public Room linkedRoom;
 
@@ -29,20 +31,20 @@ public class Door : MonoBehaviour
         switch (doorType)
         {
             case DoorType.Left:
-                player.transform.position = new Vector3(linkedRoom.rightDoor.transform.position.x - 1,
+                player.transform.position = new Vector3(linkedRoom.rightDoor.transform.position.x - WARP_OFFSET,
                     player.transform.position.y, player.transform.position.z);
                 break;
             case DoorType.Right:
-                player.transform.position = new Vector3(linkedRoom.leftDoor.transform.position.x + 1,
+                player.transform.position = new Vector3(linkedRoom.leftDoor.transform.position.x + WARP_OFFSET,
                     player.transform.position.y, player.transform.position.z);
                 break;
             case DoorType.Top:
                 player.transform.position = new Vector3(player.transform.position.x,
-                    linkedRoom.bottomDoor.transform.position.y + 1, player.transform.position.z);
+                    linkedRoom.bottomDoor.transform.position.y + WARP_OFFSET, player.transform.position.z);
                 break;
             case DoorType.Bottom:
                 player.transform.position = new Vector3(player.transform.position.x,
-                    linkedRoom.topDoor.transform.position.y - 1, player.transform.position.z);
+                    linkedRoom.topDoor.transform.position.y - WARP_OFFSET, player.transform.position.z);
                 break;
         }
     }
